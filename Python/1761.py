537069baf38d6a617a74ebc996739e82caf24655
YoungChae Na <luna20617@gmail.com>
2024-10-01 16:31:04 +0900

    10월 1일 + 수정

--- Python/1761.py
+++ Python/1761.py
@@ -1,14 +1,20 @@
 from sys import stdin, setrecursionlimit
+from collections import defaultdict
 import math
 
 setrecursionlimit(10**5) # 50,000
 
 N = int(stdin.readline())
 tree = [[] for i in range(N+1)]
+weight = defaultdict(int)
+height = [0] * (N+1) # 누적 합
+
 for i in range(N-1):
-    a, b = map(int, stdin.readline().split(' '))
+    a, b, c = map(int, stdin.readline().split(' '))
     tree[a].append(b)
     tree[b].append(a)
+    weight[(a, b)] = c
+    weight[(b, a)] = c
 
 M = int(stdin.readline())
 q = [tuple(map(int, stdin.readline().split(' '))) for i in range(M)]
@@ -20,6 +26,7 @@ level = [0] * (N + 1)
 def dfs(node, pnode):
     parent[node][0] = pnode
     level[node] = level[pnode] + 1
+    height[node] = height[pnode] + weight[(pnode, node)]
     
     for child in tree[node]:
         if child != pnode:
@@ -32,22 +39,26 @@ for k in range(1, logn): # K제곱
     for node in range(1, N+1):
         parent[node][k] = parent[parent[node][k-1]][k-1]
 
-# LCA
-for x, y in q:
+
+def lca(a, b):
     if level[x] < level[y]:
-        x, y = y, x # 5, 1
+        a, b = b, a # 5, 1
     
     for k in range(logn-1, -1, -1): # 큰수부터
-        if level[x] - (1 << k) >= level[y]:
-            x = parent[x][k]
+        if level[a] - (1 << k) >= level[b]:
+            a = parent[a][k]
     
-    if x == y:
-        print(x)
-        continue
+    if a == b:
+        return a
     
     for k in range(logn-1, -1, -1):
-        if parent[x][k] != parent[y][k]:
-            x = parent[x][k]
-            y = parent[y][k]
+        if parent[a][k] != parent[b][k]:
+            a = parent[a][k]
+            b = parent[b][k]
     
-    print(parent[x][0])
+    return parent[a][0]
+
+# LCA
+for x, y in q:
+    print(height[x] + height[y] - 2 * height[lca(x, y)])
+    
